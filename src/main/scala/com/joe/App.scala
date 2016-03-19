c08bfc3999b72764a81db2ff0e143bd2195bd90a
josiahsams <josiah_sams@in.ibm.com>
2016-03-19 13:57:27 +0530

    add random url generator

--- src/main/scala/com/joe/App.scala
+++ src/main/scala/com/joe/App.scala
@@ -25,6 +25,26 @@ object WebsiteAnalyzer {
     }
   }
 
+  def generateRandomURLs(count: Int): List[String] = {
+    val websiteBaseURL = "https://www.monitoringsite.com/"
+    val r = new scala.util.Random
+    val pageExt = ".html"
+
+    var listURL: List[String] = Nil
+
+    (1 to count).foreach(_ => {
+      import scala.util.Random
+      val sb = new StringBuilder
+      sb.append(websiteBaseURL)
+
+      Random.alphanumeric.take(10).foreach(sb.append(_))
+      //(1 to 10).foreach(sb.append(Random.alphanumeric.take(1)))
+      sb.append(pageExt)
+      listURL = sb.toString :: listURL
+    })
+
+    listURL
+  }
   def main(args: Array[String]) = {
 
     reportPageAccess("url1")
@@ -39,7 +59,9 @@ object WebsiteAnalyzer {
     reportPageAccess("url3")
     reportPageAccess("url3")
 
-    getTopNPages(31).foreach(println)
+    generateRandomURLs(10).foreach(reportPageAccess)
+
+    getTopNPages(10).foreach(println)
 
   }
 
